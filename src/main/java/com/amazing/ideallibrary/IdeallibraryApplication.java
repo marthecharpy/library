0ab23cd4e8605fbef8b4a31c3c09c725e552ccfd
marthecharpy <marthe.charpy@gmail.com>
2019-03-24 15:00:55 +0100

    first commit

---
+++ src/main/java/com/amazing/ideallibrary/IdeallibraryApplication.java
@@ -0,0 +1,13 @@
+package com.amazing.ideallibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdeallibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdeallibraryApplication.class, args);
+	}
+
+}
